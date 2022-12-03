a20ea1ffe97904c00fb6780c7e8a639300826fb0
gross2001 <eldarradle29@gmail.com>
2022-12-03 21:19:06 +0300

    a lot of changes

--- consts_dIalog.go
+++ consts_dIalog.go
@@ -1,10 +1,10 @@
 package main
 
 const (
-	onlyFiveLetter  = "Сүз 5 хәрефтән торорга тиеш"
-	wordNotExist    = "Сүзлектә мондый сүз табылмады"
-	numberOfTry     = "Синарда калды җавап бирергә мөмкинлек: "
+	onlyFiveLetter  = "Сүз 5 хәрефтән торорга тиеш "
+	wordNotExist    = "Сүзлектә мондый сүз табылмады "
+	numberOfTry     = "Сиңарда калды җавап бирергә мөмкинлек: "
 	correctWordIs   = "Дөрес сүз булды: "
-	youWin          = "Дөрес!"
-	startingNewGame = "Яңа сүз уйлап куйдым!"
+	youWin          = "Дөрес! "
+	startingNewGame = "Яңа сүз уйлап куйдым! "
 )
