354f3e8fddd9c9590079d2949ec095e0a7730ff7
E.Muliukov <eldarradle29@gmail.com>
2023-02-05 16:56:30 +0300

    delete token

--- bot/config.go
+++ bot/config.go
@@ -1,7 +1,6 @@
 package bot
 
 const (
-	//token = "5347435152:AAFtYhWbwK19LdIXypTJD6sZ-qoiGIKyHPU" // true bot
 	token             = "5882171886:AAFrDE_VMBK73c-ME12nNaV0jQaKVRbxrPw" // test
 	timeOfStart       = 1671365689                                       // unix time
 	ownerChatID int64 = 505158033
